79d9446891b59cea31faa7a621a2d16c8a6601e3
ScrubCub <cagasankylebryan@gmail.com>
2024-02-27 14:19:12 +0800

    Implement grid size changing button; new hover fx
    A button allowing for grid size changing is added
    and a new hover effect is implemented - colors are
    now randomized

--- etch.js
+++ etch.js
@@ -6,10 +6,11 @@ function createDiv() {
 
 }
 
-let body = document.querySelector('body');
-let container = document.createElement('div');
-container.setAttribute('style', 'display: flex; flex-direction: column; width: 960px; height: 960px; border: 5px solid black');
-body.appendChild(container);
+function randomColor() {
+    rand = Math.floor(Math.random()*16777215).toString(16);
+    color = '#' + rand;
+    return color;
+}
 
 function createRow(numOfRows) {
 
@@ -26,11 +27,40 @@ function createRow(numOfRows) {
             currentRow.appendChild(squareDiv);
 
             squareDiv.addEventListener('mouseover', () => {
-                squareDiv.setAttribute('style', 'display: flex; flex: 1; background-color: salmon')
+                color = randomColor();
+                squareDiv.setAttribute('style', `display: flex; flex: 1; background-color: ${color}`);
             })
         }
 
     }
 }
+
+function removeRow() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+let numOfRows = 16;
+let body = document.querySelector('body');
+let container = document.createElement('div');
+let btn = document.createElement('button');
+btn.setAttribute('style', 'font-size: 18px');
+btn.textContent = 'Change size';
+container.setAttribute('style', 'display: flex; flex-direction: column; width: 960px; height: 960px; border: 5px solid black');
+body.appendChild(btn);
+body.appendChild(container);
+
+btn.addEventListener('click', () => {
+    numOfRows = Number(prompt('Enter number of sides of the square grid: '));
+    if (numOfRows > 100) {
+        alert('Too high! Maximum limit is 100');
+        return;
+    }
+    removeRow();
+    createRow(numOfRows);
+})
+
+
 
 createRow(16);
